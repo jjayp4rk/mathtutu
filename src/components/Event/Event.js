ca071ec95c4f4fb281a013b322524fc1ecf0b160
jjayp4rk <jpark0227@gmail.com>
2018-12-30 01:20:58 -0500

    Added events

---
+++ src/components/Event/Event.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Event from "./Event";
+
+const Event = () => {
+  return (
+    <div>
+      <h1>This is a single event</h1>
+    </div>
+  );
+};
+
+export default Event;
