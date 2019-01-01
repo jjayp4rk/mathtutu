f822fb114c1d966de33117c309cabb03be74e2ef
jjayp4rk <jpark0227@gmail.com>
2019-01-01 12:05:20 -0500

    gogo

--- src/components/Event/Events.js
+++ src/components/Event/Events.js
@@ -4,7 +4,7 @@ class Events extends Component {
   render() {
     return (
       <div>
-        <h1>HERE WILL BE THE EVENTS</h1>
+        <h1>HERE WILL BE THE EVENTS zz</h1>
       </div>
     );
   }
